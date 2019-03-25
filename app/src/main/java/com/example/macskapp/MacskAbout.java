6f54047106585d18a96d18fd1dada7f3ade82ea5
SzigetiBlanka <szigetiblanka7@gmail.com>
2019-03-25 10:35:48 +0100

    activity-k létrehozása

---
+++ app/src/main/java/com/example/macskapp/MacskAbout.java
@@ -0,0 +1,13 @@
+package com.example.macskapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MacskAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_macsk_about);
+    }
+}
